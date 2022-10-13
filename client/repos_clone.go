215b092b515ecd8df7c4ff5173e3e9ea2ecfe797
Daniel Hess <dan9186@gmail.com>
2022-10-12 19:45:51 -0700

    convert clone to just use system level git

--- client/repos_clone.go
+++ client/repos_clone.go
@@ -2,13 +2,10 @@ package client
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"os"
 	"path"
-	"strings"
 
-	"github.com/go-git/go-git/v5"
+	"github.com/gomicro/align/git"
 	"github.com/gosuri/uiprogress"
 )
 
@@ -44,10 +41,17 @@ func (c *Client) CloneRepos(ctx context.Context) ([]*repository, error) {
 	for dir, rs := range dirRepos {
 		for i := range rs {
 			currRepo = fmt.Sprintf("\nCurrent Repo: %v/%v", dir, rs[i].name)
-			err := c.CloneRepo(ctx, dir, rs[i].name, rs[i].url, false)
+
+			opts := &git.CloneOptions{
+				URL:         rs[i].url,
+				Destination: path.Join(".", dir, rs[i].name),
+			}
+
+			err := c.gitClient.Clone(ctx, opts)
 			if err != nil {
 				errs = fmt.Errorf("%w; ", fmt.Errorf("clone repo: %w", err))
 			}
+
 			cloned = append(cloned, rs[i])
 			bar.Incr()
 		}
@@ -60,33 +64,4 @@ func (c *Client) CloneRepos(ctx context.Context) ([]*repository, error) {
 	}
 
 	return cloned, nil
-}
-
-// CloneRepo takes a context, base directory to clone individual repos into, the
-// name to call the repo, the url to clone the repo from, and a boolean to show
-// the output. It attempts to clone the repo into the directory structure of
-// "baseDir/name". If the repo already exists it will skip it, and otherwise
-// returns any errors it encounters.
-func (c *Client) CloneRepo(ctx context.Context, baseDir, name, url string, show bool) error {
-	dir := path.Join(".", baseDir, name)
-
-	opts := &git.CloneOptions{
-		URL:  url,
-		Auth: c.ghHTTPSAuth,
-	}
-
-	if show {
-		opts.Progress = os.Stdout
-	}
-
-	if strings.HasPrefix(url, "git@") {
-		opts.Auth = c.ghSSHAuth
-	}
-
-	_, err := git.PlainCloneContext(ctx, dir, false, opts)
-	if err != nil && !errors.Is(err, git.ErrRepositoryAlreadyExists) {
-		return fmt.Errorf("plain clone: %w", err)
-	}
-
-	return nil
 }

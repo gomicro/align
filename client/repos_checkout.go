edcd8470f47f7cd269a69fa8e4ab3bb4836950eb
dan9186 <dan9186@gmail.com>
2025-08-17 22:42:21 -0700

    try out a verbose path on checkout

--- client/repos_checkout.go
+++ client/repos_checkout.go
@@ -19,39 +19,66 @@ func (c *Client) CheckoutRepos(ctx context.Context, dirs []string, args ...strin
 	count := len(dirs)
 	args = append([]string{"checkout"}, args...)
 
+	verbose := Verbose(ctx)
+
+	var bar *uiprogress.Bar
 	currRepo := ""
-	bar := uiprogress.AddBar(count).
-		AppendCompleted().
-		PrependElapsed().
-		PrependFunc(func(b *uiprogress.Bar) string {
-			return fmt.Sprintf("Checkingout (%d/%d)", b.Current(), count)
-		}).
-		AppendFunc(func(b *uiprogress.Bar) string {
-			return currRepo
-		})
+
+	if verbose {
+		c.scrb.BeginDescribe("Command")
+		defer c.scrb.EndDescribe()
+
+		c.scrb.Print(fmt.Sprintf("git %s", strings.Join(args, " ")))
+
+		c.scrb.BeginDescribe("directories")
+		defer c.scrb.EndDescribe()
+	} else {
+		bar = uiprogress.AddBar(count).
+			AppendCompleted().
+			PrependElapsed().
+			PrependFunc(func(b *uiprogress.Bar) string {
+				return fmt.Sprintf("Checkingout (%d/%d)", b.Current(), count)
+			}).
+			AppendFunc(func(b *uiprogress.Bar) string {
+				return currRepo
+			})
+	}
 
 	unstagedRepos := []string{}
 	for _, dir := range dirs {
 		currRepo = fmt.Sprintf("\nCurrent Repo: %v", dir)
 
+		out := &bytes.Buffer{}
+		errout := &bytes.Buffer{}
+
 		cmd := exec.CommandContext(ctx, "git", args...)
-
-		buf := bytes.Buffer{}
-		cmd.Stdout = &buf
-
+		cmd.Stdout = out
+		cmd.Stderr = errout
 		cmd.Dir = dir
 
 		err := cmd.Run()
-		if err != nil {
+		if err != nil && !verbose {
 			if errors.Is(err, ErrUnstagedChanges) {
 				unstagedRepos = append(unstagedRepos, dir)
 				continue
 			}
 
-			return fmt.Errorf("checkout repo: %w", err)
+			return fmt.Errorf("checkout repo: %w", err) //TODO: collect errors and return them all
 		}
 
-		bar.Incr()
+		if verbose {
+			c.scrb.BeginDescribe(dir)
+			if err != nil {
+				c.scrb.Error(err)
+				c.scrb.PrintLines(errout)
+			} else {
+				c.scrb.PrintLines(out)
+			}
+
+			c.scrb.EndDescribe()
+		} else {
+			bar.Incr()
+		}
 	}
 
 	currRepo = ""

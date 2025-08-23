13a105bdedd006033ab2141ea96c8f70f39002cc
dan9186 <dan9186@gmail.com>
2025-08-23 15:53:11 -0700

    add client interface

---
+++ client/clienter.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+)
+
+type Clienter interface {
+	CheckoutRepos(ctx context.Context, repoDirs []string, args ...string) error
+	CloneRepos(ctx context.Context) ([]*repository, error)
+	GetDirs(ctx context.Context, dir string) ([]string, error)
+	GetLogins(ctx context.Context) ([]string, error)
+	GetRepos(ctx context.Context, name string) ([]*github.Repository, error)
+	PullRepos(ctx context.Context, repoDirs []string, args ...string) error
+	PushRepos(ctx context.Context, repoDirs []string, args ...string) error
+}

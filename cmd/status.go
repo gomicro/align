0263e790247d0253354f98c7816678ce975221ae
dan9186 <dan9186@gmail.com>
2026-04-05 12:02:15 -0700

    add branch flag to status cmd

--- cmd/status.go
+++ cmd/status.go
@@ -9,11 +9,14 @@ import (
 	"github.com/spf13/viper"
 )
 
+var showBranch bool
+
 func init() {
 	RootCmd.AddCommand(statusCmd)
 
 	statusCmd.Flags().StringVar(&dir, "dir", ".", "directory to show status of repos in")
 	statusCmd.Flags().BoolVarP(&short, "short", "s", false, "show status in short format")
+	statusCmd.Flags().BoolVarP(&showBranch, "branch", "b", false, "show branch and upstream tracking info")
 	statusCmd.Flags().BoolVar(&ignoreEmtpy, "ignore-empty", false, "ignore repos with no changes (most useful with --short)")
 }
 
@@ -37,6 +40,10 @@ func statusFunc(cmd *cobra.Command, args []string) error {
 
 	if short {
 		args = append(args, "--short")
+	}
+
+	if showBranch {
+		args = append(args, "--branch")
 	}
 
 	err = clt.StatusRepos(ctx, repoDirs, ignoreEmtpy, args...)

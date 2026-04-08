29f28e0c7cf009a635f3eea4b111b84161fdbdb6
dan9186 <dan9186@gmail.com>
2026-04-07 21:45:58 -0700

    only return dirs that are git repos

--- client/dirs.go
+++ client/dirs.go
@@ -15,9 +15,18 @@ func (c *Client) GetDirs(ctx context.Context, baseDir string) ([]string, error) 
 
 	dirs := make([]string, 0, len(files))
 	for _, f := range files {
-		if f.IsDir() {
-			dirs = append(dirs, path.Join(baseDir, f.Name()))
+		if !f.IsDir() {
+			continue
 		}
+
+		p := path.Join(baseDir, f.Name())
+
+		_, err := os.Stat(path.Join(p, ".git"))
+		if err != nil {
+			continue
+		}
+
+		dirs = append(dirs, p)
 	}
 
 	return dirs, nil

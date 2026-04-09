c5ef8c51af7065c8e50d3596474c58d98cac295e
dan9186 <dan9186@gmail.com>
2026-04-08 23:54:17 -0700

    switch the commands that behave the same over to use the fanout function

--- client/repos/branch.go
+++ client/repos/branch.go
@@ -3,14 +3,12 @@ package repos
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"os/exec"
 	"sort"
 	"strings"
 
 	ctxhelper "github.com/gomicro/align/client/context"
-	"github.com/gosuri/uiprogress"
 )
 
 func (r *Repos) ListBranches(ctx context.Context, dirs []string, args ...string) error {
@@ -49,70 +47,7 @@ func (r *Repos) ListBranches(ctx context.Context, dirs []string, args ...string)
 }
 
 func (r *Repos) Branches(ctx context.Context, dirs []string, args ...string) error {
-	count := len(dirs)
-	args = append([]string{"branch"}, args...)
-
-	verbose := ctxhelper.Verbose(ctx)
-
-	var bar *uiprogress.Bar
-	currRepo := ""
-
-	if verbose {
-		r.scrb.BeginDescribe("Command")
-		defer r.scrb.EndDescribe()
-
-		r.scrb.Print(fmt.Sprintf("git %s", strings.Join(args, " ")))
-
-		r.scrb.BeginDescribe("directories")
-		defer r.scrb.EndDescribe()
-	} else {
-		bar = uiprogress.AddBar(count).
-			AppendCompleted().
-			PrependElapsed().
-			PrependFunc(func(b *uiprogress.Bar) string {
-				return fmt.Sprintf("Branches (%d/%d)", b.Current(), count)
-			}).
-			AppendFunc(func(b *uiprogress.Bar) string {
-				return currRepo
-			})
-	}
-
-	var errs []error
-
-	for _, dir := range dirs {
-		currRepo = fmt.Sprintf("\nCurrent Repo: %v", dir)
-
-		out := &bytes.Buffer{}
-		errout := &bytes.Buffer{}
-
-		cmd := exec.CommandContext(ctx, "git", args...)
-		cmd.Stdout = out
-		cmd.Stderr = errout
-		cmd.Dir = dir
-
-		err := cmd.Run()
-		if verbose {
-			r.scrb.BeginDescribe(dir)
-			if err != nil {
-				r.scrb.Error(err)
-				r.scrb.PrintLines(errout)
-			} else {
-				r.scrb.PrintLines(out)
-			}
-
-			r.scrb.EndDescribe()
-		} else {
-			if err != nil {
-				errs = append(errs, fmt.Errorf("%s: %w: %s", dir, err, strings.TrimSpace(errout.String())))
-			}
-
-			bar.Incr()
-		}
-	}
-
-	currRepo = ""
-
-	return errors.Join(errs...)
+	return r.fanOut(ctx, dirs, "Branches", append([]string{"branch"}, args...))
 }
 
 func (r *Repos) GetBranchNames(ctx context.Context, dirs []string) ([]string, error) {
